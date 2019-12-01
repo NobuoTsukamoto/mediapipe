38c98fa66af009aaca1fc8140b2532b15f18e884
Nobuo Tsukamoto <nextremer.nb.o@gmail.com>
2019-12-01 13:54:57 +0900

    Add Jetson Nano Edge TPU example.

---
+++ mediapipe/examples/jetson_edgetpu/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+set -v
+
+echo 'Please run this from root level mediapipe directory! \n Ex:'
+echo '  sh mediapipe/examples/jetson_edgetpu/setup.sh  '
+
+sleep 3
+
+mkdir opencv32_arm64_libs
+
+cp mediapipe/examples/jetson_edgetpu/update_sources.sh update_sources.sh
+chmod +x update_sources.sh
+
+mv Dockerfile Dockerfile.orig
+cp mediapipe/examples/jetson_edgetpu/Dockerfile Dockerfile
+
+cp WORKSPACE WORKSPACE.orig
+cp mediapipe/examples/jetson_edgetpu/WORKSPACE WORKSPACE
+
